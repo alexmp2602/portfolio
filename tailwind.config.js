6128a1bf6134a068602753ee0b2cd0923f85aecb
alexmp.2602 <alexmp.2602@gmail.com>
2025-05-17 01:11:38 -0300

    Refactorizar la estructura del código para mejorar la legibilidad y el mantenimiento

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,13 @@
-/** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./src/**/*.{astro,html,js,jsx,ts,tsx}"],
+  darkMode: "class",
+  content: [
+    "./src/**/*.{astro,html,js,jsx,ts,tsx}",
+    "./components/**/*.{astro,html,js,jsx,ts,tsx}",
+    "./layouts/**/*.{astro,html,js,jsx,ts,tsx}",
+    "./pages/**/*.{astro,html,js,jsx,ts,tsx}",
+    "./**/*.astro",
+    "./**/*.js",
+  ],
   theme: {
     extend: {
       colors: {
